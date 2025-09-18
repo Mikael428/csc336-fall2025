fa5d116ed95d2925aabf8086d6b8189fac0fb21a
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-09-17 23:18:12 -0400

    change

--- 04-javascript-more/script.js
+++ 04-javascript-more/script.js
@@ -4,7 +4,21 @@
 
 const Form = document.getElementById('Form')
 const List = document.getElementById('List')
+const validationError = document.getElementById('validationError');
+const mathQuestion = document.getElementById('mathQuestion');
+const math = document.getElementById('mathinput');
 
+let correctAnswer;
+
+function MathGenerate() {
+    let num1 = randomnum(20);
+    let num2 = randomnum(20);
+    let eq_answer = num1 + num2;
+
+    const mathQuestion = document.getElementById('math-input')
+
+mathQuestion.textContent = `Answer This: ${num1} + ${num2}?`;
+}
 
 guestForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -12,37 +26,28 @@ guestForm.addEventListener("submit", function (e) {
     const name = document.getElementById('name').value;
     const colored = document.getElementById('colored').value;
     const message = document.getElementById('message').value;
+    const person_answer = parseInt('math').value;
 
+    if (eq_answer != answer) {
+    validationError.textContent = 'TRY AGAIN.';
+    return;
+    } else {
+    validationError.textContent = '';
+    }
 
     const Card = document.createElement('div');
     guestCard.classList.add('guest-card');
     guestCard.innerHTML = `
-    <h2>${name}</h2>
-    <p><strong>Address:</strong> ${colored}</p>
-    <p><strong>Message:</strong> ${message}</p>`;
+        <h2>${name}</h2>
+        <p><strong>Address:</strong> ${colored}</p>
+        <p><strong>Message:</strong> ${message}</p>
+        <p><strong>Answer:</strong> ${person_answer}</p>`;
 
     List.appendChild(Card);
     Form.reset();
+    MathGenerate();
 
 });
 
-function randomnum(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-
-}
-
-if (eq_answer != answer) {
-    validationError.textContent = 'TRY AGAIN.';
-    return;
-} else {
-    validationError.textContent = '';
-}
-
-let num1 = randomnum(20);
-let num2 = randomnum(20);
-let eq_answer = num1 + num2;
-
-const mathQuestion = document.getElementById('math-input')
-
-mathQuestion.textContent = `Answer This: ${num1} + ${num2}?`;
+MathGenerate();
 
