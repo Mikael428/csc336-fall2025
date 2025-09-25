e7dea91ba2e6c889ad51c41db753b2be2076729e
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-09-24 23:21:55 -0400

    change

---
+++ 05-external-libraries/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    var typed = new Typed('#subtitle', {
+        strings: [
+            'Look at this New Deal: Get two Large (12") Pizzas for the Price of One!!'
+        ],
+        typeSpeed: 50,
+        loop: true,
+        showCursor: true,
+        cursorChar: '|',
+    });
+});
