3a046a6d3366277e4c8c53426f80956fb6156139
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-09-28 16:30:10 -0400

    change

--- 05-external-libraries/script.js
+++ 05-external-libraries/script.js
@@ -1,11 +1,25 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    var typed = new Typed('#subtitle', {
-        strings: [
-            'Look at this New Deal: Get two Large (12") Pizzas for the Price of One!!'
-        ],
+MicroModal.init();
+
+const typedElement = document.getElementById('typed-text');
+
+if(typedElement) {
+    const typed = new Typed(typedElement, {
+        strings: ["Get 50% any large pizza!!"],
         typeSpeed: 50,
+        backSpeed: 25,
         loop: true,
-        showCursor: true,
-        cursorChar: '|',
+        Complete: (self) => {
+            if (self.el.closest('.modal__content')){
+                self.el.closest('.modal__content').style.height = 'auto';
+            }
+        }
     });
-});
+}
+
+MicroModal.show('pizza-modal');
+
+window.onload = function() {
+    const popped = this.document.getElementById("pop up");
+    popped.className = "show";
+    this.setTimeout(function(){ popped.className = popped.className.replace("show", "");}, 3000);
+}
