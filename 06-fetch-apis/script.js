7716af85e4992309fdbd5a12f8016d30fa3e0845
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-10-02 21:03:49 -0400

    change

--- 06-fetch-apis/script.js
+++ 06-fetch-apis/script.js
@@ -9,7 +9,17 @@ statusT.textContent = '';
 try {
     const repsonse = await fetch('https://official-joke-api.appspot.com/jokes/random/<any-number>');
     if (!response.ok) {
-        throw new Error()
+        throw new Error('HTTP error. Status: ${response.status}');
     }
+    const data = await response.json();
+    
+    const postTitle = data.title;
+    const userId = data.userID;
+
+    container.innerHTML = 
 }
 
+catch (error) {
+    console.error('Error:', error);
+    container.innerHTML = '<p style="color: red;">Failed to load post data. Error: ${error.message}</p>';
+}
