592478531f0ee64e2575a75ecbafe0540b4d1dc9
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-10-21 13:55:44 -0400

    change

--- 08-server-world/script.js
+++ 08-server-world/script.js
@@ -4,9 +4,8 @@ async function loadplace() {
     try {
         const res = await fetch('/world');
         const world = await res.json();
-        displayplace(place);
-    }
-    catch (err) {
+        displayplace(world);
+    } catch (err) {
         console.error('Error...', err);
         document.getElementById('worlds').textContent = 'Failed to load';
     }
@@ -16,9 +15,31 @@ function displayplace(place) {
     const contained = document.getElementById('worlds');
     contained.innerHTML = '';
 
-    if (!world.towns) {
+    if (!place.towns) {
         contained.textContent = 'Nothing Found.';
         return;
     }
-    
+
+
+place.towns.forEach(town => {
+    const townD = document.createElement('div');
+    townD.classList.add('town');
+    townD.innerHTML = `<h3>${town.name}</h3>`;
+
+
+    if (town.people && town.people.length > 0) {
+        const ul = document.createElement('ul');
+        town.people.forEach(p => {
+            const li = document.createElement('li');
+            li.textContent = `${p.name} ${p.item}`
+            ul.appendChild(li);
+        });
+        townD.appendChild(ul);
+    } else {
+        townD.innerHTML += '<p>No one as of now.<p>';
+    }
+
+    contained.appendChild(townD);
+});
 }
+
