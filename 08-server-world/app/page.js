8ab727b8c7c81e6cd7578d8556ca2174a879d5e4
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-10-28 17:09:38 -0400

    change

--- 08-server-world/app/page.js
+++ 08-server-world/app/page.js
@@ -1,3 +1,33 @@
 export default function Page() {
-    return <h1>Hello from Next.js!</h1>;
-}
+    return (
+        <div>
+            {/*
+        <title>My Website</title>
+            */}
+            <h1>Information: </h1>
+
+
+            <div id="worlds">
+                <p>Loading...</p>
+            </div>
+
+            <h2>Put more Here: </h2>
+
+            <form id="add-peoples">
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" required />
+
+                <label htmlFor="name">Town:</label>
+                <input type="text" name="town" required />
+
+                <label htmlFor="name">Item:</label>
+                <input type="text" name="item" optional />
+
+                <button type="submit">Add: </button>
+            </form>
+
+            <p id="status"></p>
+            <h1>Hello from Next.js!</h1>
+        </div>
+        );
+    }
