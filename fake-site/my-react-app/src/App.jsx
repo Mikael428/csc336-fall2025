a726bf58450cc8773843a430c282728882c18f1d
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-11-30 21:56:08 -0500

    change

--- fake-site/my-react-app/src/App.jsx
+++ fake-site/my-react-app/src/App.jsx
@@ -1,32 +1,7 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-import DogImageFetcher from "./DogImageFetcher.jsx";
 import "./App.css";
 
-function Home() {
-  return (
-    <div>
-      <h1>Welcome to Dog Pics</h1>
-      <p>Click “Random Dog” in the nav to see a random dog photo.</p>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <BrowserRouter>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/dogs">Random Dog</NavLink>
-      </nav>
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dogs" element={<DogImageFetcher />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <h1>Hello from App</h1>;
 }
 
 export default App;
--- final/client/public/src/App.jsx
+++ final/client/public/src/App.jsx
@@ -1,18 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../../../../fake-site/my-react-app/src/Home";
-import Register from "../../../../fake-site/my-react-app/src/Register";
-import Schedule from "../../../../fake-site/my-react-app/src/Schedule";
+import "./App.css";
 
 function App() {
-    return (
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="schedule" element={<Home />} />
-            <Route path="register" element={<Home />} />
-            </Routes>
-            </BrowserRouter>
-    );
+  return <h1>Hello from App</h1>;
 }
 
 export default App;
