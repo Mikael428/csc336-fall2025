bba826b55f8df9979931c9514bcf485cd63268a7
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-12-01 12:39:33 -0500

    change

--- fake-site/my-react-app/src/App.jsx
+++ fake-site/my-react-app/src/App.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; // CRITICAL: Import all router components
+
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"; // CRITICAL: Import all router components
 import './App.css'; 
 
-// CRITICAL: Use simple relative paths assuming all components are in the same 'src/' directory
 import Home from './Home'; 
-import Register from './Register';
-import Schedule from './Schedule';
+import Register from './Register.jsx';
+import Schedule from './Schedule.jsx';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Navigation Bar (visible on all pages) */}
+
       <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/schedule">Schedule</Link>
-          <Link to="/register">Register</Link>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/schedule">Schedule</NavLink>
+          <NavLink to="/register">Register</NavLink>
       </nav>
 
-      <div className="content-container">
-        {/* Routes define which component to render based on the URL */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="schedule" element={<Schedule />} />
           <Route path="register" element={<Register />} />
         </Routes>
-      </div>
     </BrowserRouter>
-  );
+    
+  )
 }
 
 export default App;
