11ca5f92809bc728ae49e73e71038f84639ce218
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-12-04 19:28:20 -0500

    change

--- final-2/my-react-app/src/Home.jsx
+++ final-2/my-react-app/src/Home.jsx
@@ -4,7 +4,6 @@
   <div>
     <h1>Home Page</h1>
     <p>Welcome! Our Company is brand new, but ready to make your experience! Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few pianoforte discovered uncommonly ask. Morning cousins amongst in mr weather do neither. Warmth object matter course active law spring six. Pursuit showing tedious unknown winding see had man add. And park eyes too more him. Simple excuse active had son wholly coming number add. Though all excuse ladies rather regard assure yet. If feelings so prospect no as raptures quitting.How promotion excellent curiosity yet attempted happiness. Gay prosperous impression had conviction. For every delay death ask style. Me mean able my by in they. Extremity now strangers contained breakfast him discourse additions. Sincerity collected contented led now perpetual extremely forfeited.</p>
-
   </div>
 );
 }
