2ffcf931c93a2b209388d1eb3f9a6524a3d76197
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-12-08 22:23:21 -0500

    change

--- final/server/scheduleRoutes.js
+++ final/server/scheduleRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import * as scheduleController from 'scheduleController.js';
+import * as scheduleController from './scheduleController.js';
 
 const router = express.Router();
 
-router.get('/schedule', scheduleController, getSchedule);
+router.get('/schedule', scheduleController.getSchedule);
 
 export default router;
