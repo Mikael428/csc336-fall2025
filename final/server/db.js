e140cea396fe6de19fe77485ed41e8eb9775f225
Mikael428 <156851715+Mikael428@users.noreply.github.com>
2025-12-11 10:51:31 -0500

    change

--- final/server/db.js
+++ final/server/db.js
@@ -1,20 +1,20 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const connectDB = async () => {
-    const uri = process.env.MONGO_URI;
+// const connectDB = async () => {
+//     const uri = process.env.MONGO_URI;
 
-if (!uri) {
-        console.error("FATAL ERROR: MONGO_URI is not defined.");
-        return;
-}
+// if (!uri) {
+//         console.error("FATAL ERROR: MONGO_URI is not defined.");
+//         return;
+// }
 
-try {
-        await mongoose.connect(uri);
-        console.log('MongoDB Connected Successfully!');
-    } catch (err) {
-        console.error(`MongoDB Connection Failed: ${err.message}`);
-        process.exit(1);
-    }
-};
+// try {
+//         await mongoose.connect(uri);
+//         console.log('MongoDB Connected Successfully!');
+//     } catch (err) {
+//         console.error(`MongoDB Connection Failed: ${err.message}`);
+//         process.exit(1);
+//     }
+// };
 
-export default connectDB;
+// export default connectDB;
